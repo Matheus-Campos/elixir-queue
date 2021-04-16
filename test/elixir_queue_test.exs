583d96c013815ceb97c096538289e53c2820b5e3
Matheus Campos da Silva <matheus.campos@compasso.com.br>
2021-04-16 10:55:01 -0300

    test: add tests to queue

--- test/elixir_queue_test.exs
+++ test/elixir_queue_test.exs
@@ -1,8 +1,36 @@
 defmodule ElixirQueueTest do
   use ExUnit.Case
-  doctest ElixirQueue
 
-  test "greets the world" do
-    assert ElixirQueue.hello() == :world
+  test "should start a queue" do
+    assert {:ok, pid} = ElixirQueue.start_link([])
+    assert Process.alive?(pid)
+  end
+
+  test "should successfully enqueue an element" do
+    {:ok, pid} = ElixirQueue.start_link([])
+
+    element = {:hello, :world}
+    response = ElixirQueue.enqueue(pid, element)
+
+    expected_response = :ok
+
+    assert response == expected_response
+  end
+
+  test "should successfully dequeue an element" do
+    element = 1
+    {:ok, pid} = ElixirQueue.start_link([element])
+
+    response = ElixirQueue.dequeue(pid)
+
+    assert response == element
+  end
+
+  test "should successfully dequeue an element in an empty queue" do
+    {:ok, pid} = ElixirQueue.start_link([])
+
+    response = ElixirQueue.dequeue(pid)
+
+    assert response == nil
   end
 end
