1385b0b43b355e75b2157f2f98c0f1d73b7dffa2
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-04-16 09:03:17 -0300

    chore: start project

---
+++ lib/elixir_queue.ex
@@ -0,0 +1,18 @@
+defmodule ElixirQueue do
+  @moduledoc """
+  Documentation for `ElixirQueue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirQueue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
