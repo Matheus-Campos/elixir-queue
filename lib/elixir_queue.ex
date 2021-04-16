1d00a0c6b8d79ec628ca95496ab3e0a2a1434919
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-04-16 10:07:40 -0300

    feat: WIP!

--- lib/elixir_queue.ex
+++ lib/elixir_queue.ex
@@ -1,18 +1,33 @@
 defmodule ElixirQueue do
-  @moduledoc """
-  Documentation for `ElixirQueue`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirQueue.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_link(initial_state) do
+    GenServer.start_link(__MODULE__, initial_state)
   end
+
+  def enqueue(pid, element) do
+    GenServer.cast(pid, {:enqueue, element})
+  end
+
+  def dequeue(pid) do
+    GenServer.call(pid, :dequeue)
+  end
+
+  @impl true
+  def init(initial_state) do
+    GenServer.start_link(__MODULE__, initial_state)
+  end
+
+  @impl true
+  def handle_cast({:enqueue, element}, state) do
+    {:noreply, List.insert_at(state, length(state), element)}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
 end
